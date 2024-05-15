3f5b9907f5d6b725f67b3d070cd0b6f663c6ce50
Sifa Game <sifa@sifa.game>
2024-05-15 10:41:26 +0300

    Simplify SIFA token

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import {IERC20} from "./SifaToken.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract Vault is ReentrancyGuard {
