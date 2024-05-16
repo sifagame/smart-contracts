092017e87b120c12786895b738bfa18f48366c88
Sifa <163406591+sifagame@users.noreply.github.com>
2024-05-16 15:03:50 +0300

    Deployment scripts (#16)
    * Ignition modules and deploy script
    
    * Test deploy in GitHub Actions
    
    * Remove hardhat-ganache
    
    * More deployment settings
    
    * Use secrects

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,32 @@
+require("dotenv").config();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.24",
-	settings: {
-		optimizer: {
-		  enabled: true,
-		  runs: 1000,
-		},
-	  },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    testnet: {
+      url: "https://sepolia-rollup.arbitrum.io/rpc",
+      chainId: 421614,
+      accounts: [process.env.TESTNET_DEPLOYER_PRIMARY_KEY || ""],
+    },
+    mainnet: {
+      url: "https://arb1.arbitrum.io/rpc",
+      chainId: 42161,
+      accounts: [process.env.MAINNET_DEPLOYER_PRIMARY_KEY || ""],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ARBISCAN_API_KEY,
   },
 };
 
