e3a46869adacc896b910c2f3a43ca899e86c4e9a
Sifa Game <sifa@sifa.game>
2024-05-25 15:28:42 +0300

    Fix deploy error

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -96,7 +96,7 @@ async function main() {
       Emitter: {
         owner: await emitterOwner.getAddress(),
         token: tokenAddress,
-        //vault: vaultAddress,
+        vault: vaultAddress,
       },
     },
   });
