8ddeb5745e1f8f315c5f7d9d0e8f40e62291626c
Sifa Game <sifa@sifa.game>
2024-05-27 13:34:41 +0300

    Vault: use redeem only, add 2-week delay

--- test/Vault.ts
+++ test/Vault.ts
@@ -24,16 +24,126 @@ describe("Vault", function () {
     });
   });
 
+  describe("Redeem Request", () => {
+    it("Should request redeem", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+
+      const shares = await vault.balanceOf(owner);
+      await vault.requestRedeem(shares, owner);
+      const request = await vault.redeemRequests(owner);
+      const now = await time.latest();
+      const twoWeeks = now + 2 * 7 * 24 * 60 * 60;
+      expect(request[0]).equals(twoWeeks);
+      expect(request[1]).equals(shares);
+    });
+
+    it("Should revert if shares exceed", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+
+      const shares = ethers.parseEther("1.01");
+      await expect(vault.requestRedeem(shares, owner)).to.be.revertedWith(
+        "Exceeds max redeem"
+      );
+    });
+
+    it("Request with allowance", async () => {
+      const { vault, sifa } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      const [owner, otherAccount] = await ethers.getSigners();
+
+      await vault.deposit(assets, otherAccount);
+
+      const shares = await vault.balanceOf(otherAccount);
+      await expect(
+        vault.requestRedeem(shares, otherAccount)
+      ).to.be.revertedWith("Not enough allowance");
+
+      await vault.connect(otherAccount).approve(owner, shares);
+      const now = await time.latest();
+      const twoWeeks = now + 2 * 7 * 24 * 60 * 60 + 1;
+
+      await expect(vault.requestRedeem(shares, otherAccount))
+        .to.emit(vault, "RedeemRequested")
+        .withArgs(shares, otherAccount, twoWeeks);
+    });
+  });
+
+  describe("Withdraw", () => {
+    it("Withdraw disabled", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+      await expect(vault.withdraw(assets, owner, owner)).to.be.revertedWith(
+        "Use redeem()"
+      );
+    });
+  });
+
+  describe("Redeem", () => {
+    it("Should revert before time", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+      await vault.requestRedeem(assets, owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt - 5n);
+
+      await expect(vault.redeem(shares, owner, owner)).to.be.revertedWith(
+        "Redeem not available yet"
+      );
+    });
+
+    it("Should revert amount exceeds", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+      await vault.requestRedeem(assets, owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt + 10n);
+      await expect(
+        vault.redeem(shares + 100n, owner, owner)
+      ).to.be.revertedWith("Redeem amount exceed available");
+    });
+
+    it("Should redeem", async () => {
+      const { vault, sifa, owner } = await loadFixture(deployVault);
+      const assets = ethers.parseEther("1");
+      await sifa.approve(vault, assets);
+      await vault.deposit(assets, owner);
+      await vault.requestRedeem(assets, owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt + 10n);
+      await expect(vault.redeem(shares, owner, owner))
+        .to.emit(vault, "Withdraw")
+        .withArgs(owner, owner, owner, assets, shares);
+    });
+  });
+
   describe("Connect with emitter", () => {
     it("Should skip emitter if not set", async () => {
       const { emitter, vault, sifa, owner } = await loadFixture(deployEmitter);
       await sifa.approve(vault, ethers.parseEther("1000"));
       await vault.deposit(ethers.parseEther("1000"), owner);
+      await vault.requestRedeem(ethers.parseEther("1000"), owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt + 10n);
+
       await sifa.transfer(vault, ethers.parseEther("1000"));
-      const maxWithdraw = await vault.maxWithdraw(owner);
-	  await expect(vault.withdraw(maxWithdraw/2n, owner, owner)).to.not.emit(emitter, "Withdrawn");
-	  const maxRedeem = await vault.maxRedeem(owner);
-	  await expect(vault.redeem(maxRedeem, owner, owner)).to.not.emit(emitter, "Withdrawn");
+      const maxRedeem = await vault.maxRedeem(owner);
+      await expect(vault.redeem(maxRedeem, owner, owner)).to.not.emit(
+        emitter,
+        "Withdrawn"
+      );
     });
 
     it("Should update emitter", async () => {
@@ -55,18 +165,16 @@ describe("Vault", function () {
       await vault.deposit(assets, owner);
 
       const maxRedeem = await vault.maxRedeem(owner);
-      await expect(vault.redeem(maxRedeem, owner, owner)).to.emit(
+      await vault.requestRedeem(maxRedeem, owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt + 10n);
+
+      await expect(vault.redeem(shares, owner, owner)).to.emit(
         emitter,
         "Withdrawn"
       );
 
       await vault.deposit(assets, owner);
-
-      const maxWithdraw = await vault.maxWithdraw(owner);
-      await expect(vault.withdraw(maxWithdraw, owner, owner)).to.emit(
-        emitter,
-        "Withdrawn"
-      );
 
       const assetsAfter = await sifa.balanceOf(owner);
 
@@ -81,38 +189,39 @@ describe("Vault", function () {
       const epochLength = await emitter.epochLength();
       const [owner, otherAccount] = await ethers.getSigners();
 
-      const otherAsssets = ethers.parseEther("4.2");
+      const otherAsets = ethers.parseEther("4.2");
 
       await emitter.withdraw();
 
       await time.increase(epochLength * 100n);
-      await vault.deposit(otherAsssets, otherAccount);
-
-      await time.increase(epochLength * 50n);
-
+      await vault.deposit(otherAsets, otherAccount);
       const ownerShares = await vault.balanceOf(owner);
       const otherShares = await vault.balanceOf(otherAccount);
+
+      await vault.requestRedeem(ownerShares, owner);
+      await vault
+        .connect(otherAccount)
+        .requestRedeem(otherShares, otherAccount);
+
+      await time.increase(epochLength * 50n);
 
       await vault.redeem(ownerShares, owner, owner);
       await vault
         .connect(otherAccount)
         .redeem(otherShares, otherAccount, otherAccount);
 
-      await vault.deposit(otherAsssets, owner);
-      await vault.deposit(otherAsssets, otherAccount);
+      await vault.deposit(otherAsets, owner);
 
-      await sifa.transfer(vault, otherAsssets);
+      await sifa.transfer(vault, otherAsets);
+      const maxRedeem = await vault.maxRedeem(owner);
+      await vault.requestRedeem(maxRedeem, owner);
+      const [availableAt, shares] = await vault.redeemRequests(owner);
+      await time.increaseTo(availableAt + 10n);
 
       await expect(vault.redeem(30000000n, owner, owner)).to.emit(
         vault,
         "Withdraw"
       );
-
-	  await expect(vault.withdraw(30000000n, owner, owner)).to.emit(
-        vault,
-        "Withdraw"
-      );
-
     });
   });
 });
