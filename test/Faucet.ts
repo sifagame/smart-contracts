fb686492ce1a98516bac28f8dff4670bb230cee0
Sifa <163406591+sifagame@users.noreply.github.com>
2024-05-28 11:15:34 +0300

    Faucet claim() and nextClaimAt() functions (#30)

--- test/Faucet.ts
+++ test/Faucet.ts
@@ -15,10 +15,11 @@ describe("Faucet", () => {
       expect(await faucet.TOKEN()).equals(sifa);
       expect(await faucet.DROP_AMOUNT()).equals(420);
       expect(await faucet.DELAY()).equals(60);
+      expect(await faucet.REQUIRE_ETH()).equals(ethers.parseEther("0.001"));
     });
   });
 
-  describe("Drop", () => {
+  describe("Errors", () => {
     it("Require ETH", async () => {
       const { faucet, sifa, owner } = await loadFixture(deployFaucet);
       sifa.transfer(faucet, ethers.parseEther("1000000"));
@@ -28,40 +29,70 @@ describe("Faucet", () => {
         to: user,
         value: ethers.parseEther("0.0009"),
       });
-      await expect(faucet.drop(user)).to.be.revertedWith("Own more 0.001 ETH");
+      await expect(faucet.drop(user))
+        .to.be.revertedWithCustomError(faucet, "FaucetNotEnoughETH")
+        .withArgs(user);
     });
 
+    it("Not enough tokens", async () => {
+      const { faucet, sifa, owner } = await loadFixture(deployFaucet);
+      sifa.transfer(faucet, ethers.parseEther("839"));
+
+      expect(await faucet.available(owner)).equals(true);
+      await expect(faucet.connect(owner).drop(owner))
+        .to.emit(faucet, "Dropped")
+        .withArgs(ethers.parseEther("420"), owner);
+
+      await time.increase(60 * 60 * 24);
+
+      await expect(faucet.connect(owner).drop(owner))
+        .to.be.revertedWithCustomError(faucet, "FaucetHasNotEnoughTokens")
+        .withArgs(ethers.parseEther("419"));
+    });
+  });
+
+  describe("Drop", () => {
     it("Should drop with schedule", async () => {
       const { faucet, sifa, otherAccount } = await loadFixture(deployFaucet);
 
       sifa.transfer(faucet, ethers.parseEther("1000000"));
 
       expect(await faucet.available(otherAccount)).equals(true);
-      await expect(faucet.connect(otherAccount).drop(otherAccount)).to.emit(
-        faucet,
-        "Dropped"
-      );
-      expect(await sifa.balanceOf(otherAccount)).equals(
-        ethers.parseEther("420")
-      );
+      await expect(faucet.connect(otherAccount).drop(otherAccount))
+        .to.emit(faucet, "Dropped")
+        .withArgs(ethers.parseEther("420"), otherAccount);
 
       await time.increase(100);
 
       expect(await faucet.available(otherAccount)).equals(false);
-      await expect(
-        faucet.connect(otherAccount).drop(otherAccount)
-      ).to.be.revertedWith("Wait");
+      await expect(faucet.connect(otherAccount).drop(otherAccount))
+        .to.be.revertedWithCustomError(faucet, "FaucetClaimNotAvailable")
+        .withArgs(otherAccount);
 
       await time.increase(60 * 60 * 24);
 
       expect(await faucet.available(otherAccount)).equals(true);
-      await expect(faucet.connect(otherAccount).drop(otherAccount)).to.emit(
-        faucet,
-        "Dropped"
-      );
+      await expect(faucet.connect(otherAccount).drop(otherAccount))
+        .to.emit(faucet, "Dropped")
+        .withArgs(ethers.parseEther("420"), otherAccount);
+
       expect(await sifa.balanceOf(otherAccount)).equals(
         ethers.parseEther("840")
       );
+    });
+  });
+
+  describe("AvailableAt", () => {
+    it("Should return correct time", async () => {
+      const { faucet, sifa, owner } = await loadFixture(deployFaucet);
+      sifa.transfer(faucet, ethers.parseEther("1000000"));
+
+      expect(await faucet.nextClaimAt(owner)).equals(0);
+      await expect(faucet.claim())
+        .to.emit(faucet, "Dropped")
+        .withArgs(ethers.parseEther("420"), owner);
+      const latest = await time.latest();
+      expect(await faucet.nextClaimAt(owner)).equals(latest + 60 * 60 * 24);
     });
   });
 });
