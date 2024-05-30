89bb452572057739a70766416157d33aeb8abce0
Sifa <163406591+sifagame@users.noreply.github.com>
2024-05-30 21:56:36 +0300

    Revert "Vault: use redeem only, add 2-week delay (#27)" (#32)
    This reverts commit a6867a5a674aabd40974ca2a7f3f3c02d46513de.

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -7,32 +7,10 @@ import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.so
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IEmitter} from "./Emitter.sol";
 
-import "hardhat/console.sol";
-
 contract Vault is Ownable, ERC4626 {
     event EmitterUpdated(address emitter);
-    event RedeemRequested(uint256 shares, address owner, uint256 availableAt);
 
-    struct RedeemRequest {
-        uint256 availableAt;
-        uint256 shares;
-    }
-
-    uint256 constant REDEEM_DELAY = 14 days;
     address public emitter;
-    mapping(address owner => RedeemRequest) public redeemRequests;
-
-    modifier canRedeem(uint256 shares, address owner) {
-        require(
-            redeemRequests[owner].shares >= shares,
-            "Redeem amount exceed available"
-        );
-        require(
-            redeemRequests[owner].availableAt <= block.timestamp,
-            "Redeem not available yet"
-        );
-        _;
-    }
 
     constructor(
         IERC20 token_,
@@ -45,45 +23,28 @@ contract Vault is Ownable, ERC4626 {
     }
 
     function withdraw(
-        uint256,
-        address,
-        address
-    ) public virtual override returns (uint256) {
-        revert("Use redeem()");
-    }
-
-    function requestRedeem(
-        uint256 shares,
+        uint256 assets,
+        address receiver,
         address owner
-    ) public virtual returns (RedeemRequest memory) {
-        require(shares <= maxRedeem(owner), "Exceeds max redeem");
-        if (_msgSender() != owner) {
-            require(
-                shares <= allowance(owner, _msgSender()),
-                "Not enough allowance"
-            );
+    ) public virtual override returns (uint256) {
+        if (address(0) != emitter) {
+            if (IEmitter(emitter).available() > 0) {
+                IEmitter(emitter).withdraw();
+            }
         }
-
-        uint256 newTime = block.timestamp + REDEEM_DELAY;
-
-        redeemRequests[owner].availableAt = newTime;
-        redeemRequests[owner].shares = shares;
-
-        emit RedeemRequested(shares, owner, newTime);
-        return redeemRequests[owner];
+        return super.withdraw(assets, receiver, owner);
     }
 
     function redeem(
         uint256 shares,
         address receiver,
         address owner
-    ) public virtual override canRedeem(shares, owner) returns (uint256) {
+    ) public virtual override returns (uint256) {
         if (address(0) != emitter) {
             if (IEmitter(emitter).available() > 0) {
                 IEmitter(emitter).withdraw();
             }
         }
-		redeemRequests[owner].shares -= shares;
         return super.redeem(shares, receiver, owner);
     }
 }
